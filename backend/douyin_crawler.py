b2ea03ad3517966ff787d3d38dfbb27b0d44e323
fangweihao123 <fwh19970312@gmail.com>
2025-05-12 10:56:56 +0800

    docker更新 使用nginx

--- backend/douyin_crawler.py
+++ backend/douyin_crawler.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
-import time
+import time 
 import json
 import re
 
