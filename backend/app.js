6ee175c1cfe56db1d8df67855ef08086dccfcb62
fangweihao123 <fwh19970312@gmail.com>
2025-05-13 15:33:31 +0800

    mac电脑的部署以及获取accesstoken

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ app.use(express.json());
 // 抖音开放平台配置
 const DOUYIN_CLIENT_KEY = process.env.DOUYIN_CLIENT_KEY || 'aw48uuo6r8xm48xb';
 const DOUYIN_CLIENT_SECRET = process.env.DOUYIN_CLIENT_SECRET || '7ef22bcf82420090464ee81c7f1e0651';
-const DOUYIN_REDIRECT_URI = 'http://localhost:5000/api/callback';
+const DOUYIN_REDIRECT_URI = 'https://cd04-240e-391-cda-6bf0-c9de-59a0-c469-b5cb.ngrok-free.app/auth-success';
 
 // 数据库配置
 const sequelize = new Sequelize({
@@ -63,7 +63,7 @@ const AuthInfo = sequelize.define('AuthInfo', {
 // 获取授权URL
 app.get('/api/auth', (req, res) => {
     try {
-        const authUrl = `https://open.douyin.com/platform/oauth/connect?client_key=aw48uuo6r8xm48xb&response_type=code&scope=trial.whitelist&redirect_uri=https://756d-116-148-240-41.ngrok-free.app/auth-success`;
+        const authUrl = `https://open.douyin.com/platform/oauth/connect?client_key=aw48uuo6r8xm48xb&response_type=code&scope=trial.whitelist&redirect_uri=${DOUYIN_REDIRECT_URI}`;
         res.json({ authUrl });
     } catch (error) {
         logger.error(`Error Get AuthUrl: ${error.message}`);
@@ -85,7 +85,7 @@ app.post('/api/auth/callback', async (req, res) => {
         });
         const messageRes = await client.oauthAccessToken(params);
         if(messageRes.message === 'success'){
-            console.log('Success receive access token', messageRes.data.access_token);
+            console.log('Success receive access token', messageRes.data);
         }else{
             console.log('Error response from douyin', messageRes.data);
         }
@@ -101,7 +101,7 @@ app.post('/api/auth/callback', async (req, res) => {
 });
 
 // 启动服务器
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Starting Express application...`);
     logger.info(`Server is running on port ${PORT}`);
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
     port: 8080,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:5001',
         changeOrigin: true,
         pathRewrite: {
           '^/api': '/api'
